8a60d04293827fa46677e88fffb2d4f05dee8919
Jakub Šmrha <kuba.smrha@gmail.com>
2019-12-01 15:09:52 +0100

    Added lots of settings. Adding Admin GUI's. Added a defaults generating CMD. Moved profession settings from config to a dedicated file.
    Signed-off-by: Jakub Šmrha <kuba.smrha@gmail.com>

--- src/git/doomshade/professions/data/AbstractSettings.java
+++ src/git/doomshade/professions/data/AbstractSettings.java
@@ -9,6 +9,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import java.util.logging.Level;
 
 public abstract class AbstractSettings implements ISetup {
+    private static final Level LEVEL = Level.WARNING;
     protected static FileConfiguration config;
     private static Professions plugin = Professions.getInstance();
 
@@ -25,12 +26,12 @@ public abstract class AbstractSettings implements ISetup {
     }
 
     protected void printError(String section, Object value) {
-        Professions.log("Your configuration file is outdated!", Level.CONFIG);
-        Professions.log(String.format("Missing \"%s\" section!", section), Level.CONFIG);
+        Professions.log("Your configuration file is outdated!", LEVEL);
+        Professions.log(String.format("Missing \"%s\" section!", section), LEVEL);
         if (value == null)
-            Professions.log("Using default values.", Level.CONFIG);
+            Professions.log("Using default values.", LEVEL);
         else
-            Professions.log(String.format("Using %s as default value.", value.toString()), Level.CONFIG);
+            Professions.log(String.format("Using %s as default value.", value.toString()), LEVEL);
     }
 
     protected final boolean isSection(String section, Object value) {
@@ -49,6 +50,7 @@ public abstract class AbstractSettings implements ISetup {
 
     @Override
     public void setup() throws ConfigurationException {
+        loadConfig();
         assertSectionExists();
     }
 
